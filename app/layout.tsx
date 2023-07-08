2dcf9f72e8c045c39bed181dd8a46ced390d723a
kotiasv <vshakitskiy@gmail.com>
2023-07-08 15:11:19 +0300

    fix use client problem

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,17 @@
 "use client"
 
-import { Metadata } from "next"
 import { ReactNode, useState } from "react"
 import "./globals.css"
 import { ThemeContext } from "@/hooks/theme"
-
-export const metadata: Metadata = {
-    title: "kotiasv",
-    description: "My personal website!",
-}
 
 const Layout = ({ children }: { children: ReactNode }) => {
     const [darkMode, setDarkMode] = useState(true)
     return (
         <html className={`${darkMode ? "dark" : ""}`}>
+            <head>
+                <title>kotiasv</title>
+                <meta name="description" content="My personal website!" />
+            </head>
             <ThemeContext.Provider value={{ darkMode, setDarkMode }}>
                 <body className="bg-[#f9f9f9] text-back dark:bg-back dark:text-white">
                     {children}
